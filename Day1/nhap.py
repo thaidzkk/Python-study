bddbe701c672f75f16e9adefbf88570cb44e9af8
thaidzkk <tranquangthaikk147@gmail.com>
2025-10-08 22:12:00 +0700

    init commit

---
+++ Day1/nhap.py
@@ -0,0 +1,9 @@
+print("Hello, World!aa")
+# This is a comment
+print("Welcome to Python programming.")
+print("Một số tiếng Việt có dấuaaa.aa")
+print("❤")
+for i in range(5):
+    print(i)
+print("End of the program.")    
+ 
